436435227855c097ebd15b87376e916ac8f8fe58
manulzvz13 <manulzvz@gmail.com>
2025-07-23 14:27:50 -0600

    fix(dark icon): Modify non existent SVG path with moon svg

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // ===== Toggle Dark Mode =====
   const themeToggle = document.getElementById("theme-toggle");
-  const themeIcon = document.getElementById("theme-icon");
+  const themeSvg = themeToggle.querySelector("svg");
+  const themePath = themeSvg.querySelector("path");
 
   themeToggle.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
 
     const isDark = document.body.classList.contains("dark-mode");
-    themeIcon.setAttribute(
+    themePath.setAttribute(
       "d",
       isDark
-        ? "M480-80q-83 0-156.5-31.5T192-200q-55-55-86.5-128.5T74-480q0-83 31.5-156.5T192-765q9-9 22.5-9t22.5 9q9 9 9 22.5T237-720q-46 46-71.5 106T140-480q0 114 83 197t197 83q68 0 128-29t106-75q9-9 22.5-9t22.5 9q9 9 9 22.5t-9 22.5q-61 61-134.5 93.5T480-80Z"
-        : "M440-40v-240h80v240h-80Zm0-640v-240h80v240h-80ZM200-226l-56-56 84-84 56 56-84 84Zm440-440-56-56 84-84 56 56-84 84ZM40-440v-80h240v80H40Zm640 0v-80h240v80H680ZM200-694l84-84 56 56-84 84-56-56Zm440 440 84-84 56 56-84 84-56-56ZM480-280q-83 0-141.5-58.5T280-480q0-83 58.5-141.5T480-680q83 0 141.5 58.5T680-480q0 83-58.5 141.5T480-280Z"
+        ? "M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Zm0-80q88 0 158-48.5T740-375q-20 5-40 8t-40 3q-123 0-209.5-86.5T364-660q0-20 3-40t8-40q-78 32-126.5 102T200-480q0 116 82 198t198 82Zm-10-270Z"
+        : "M40-440v-80h240v80H40Zm270-154-84-84 56-56 84 84-56 56Zm130-86v-240h80v240h-80Zm210 86-56-56 84-84 56 56-84 84Zm30 154v-80h240v80H680Zm-200 80q-50 0-85-35t-35-85q0-50 35-85t85-35q50 0 85 35t35 85q0 50-35 85t-85 35Zm198 134-84-84 56-56 84 84-56 56Zm-396 0-56-56 84-84 56 56-84 84ZM440-40v-240h80v240h-80Z"
     );
   });
 
